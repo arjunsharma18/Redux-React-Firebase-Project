148c909997dd80a3dbe0ca3ed1d3ff6acbbfba3d
arjunsharma18 <arjun.sharmaa2019@gmail.com>
2022-02-08 18:46:25 +0530

    login and register page completed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const LoginPage = () => {
-    return <div>
-        LoginPage
+function LoginPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [cpassword, setCPassword] = useState('')
+    return <div className='login-parent'>
+        <div className='row justify-content-center'>
+            <div className='col-md-5'>
+                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_hu9cd9.json" background="transparent" speed="1" loop autoplay></lottie-player>
+            </div>
+            <div className='col-md-4'>
+                <div className='login-form'>
+                    <h2>Login</h2>
+                    <hr></hr>
+                    <input type="text" className='form-control' placeholder='email' value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
+                    <input type="text" className='form-control' placeholder='password' value={email} onChange={(e) => { setPassword(e.target.value) }}></input>
+
+                    <button className='my-3'>Login</button>
+                    <hr>
+                    </hr>
+                    <Link to='/register'>Click here to Register</Link>
+                </div>
+            </div>
+        </div>
     </div>;
-};
+}
 
 export default LoginPage;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [cpassword, setCPassword] = useState('')
     return <div className='register-parent'>
-        <div className='row'>
+        <div className='row justify-content-center'>
             <div className='col-md-5'>
                 <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_h7ofe9pc.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
             <div className='col-md-4'>
-                <div className='login-form'>
+                <div className='register-form'>
                     <h2>Register</h2>
                     <hr></hr>
                     <input type="text" className='form-control' placeholder='email' value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
                     <input type="text" className='form-control' placeholder='password' value={email} onChange={(e) => { setPassword(e.target.value) }}></input>
                     <input type="text" className='form-control' placeholder='confirm password' value={email} onChange={(e) => { setCPassword(e.target.value) }}></input>
-                    <button>REGISTER</button>
+                    <button className='my-3'>REGISTER</button>
+                    <hr>
+                    </hr>
+                    <Link to='/login'>Click here to Login</Link>
                 </div>
             </div>
         </div>
